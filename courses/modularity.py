9562ec9ed25b672c5809f06ad345101c23030fec
Anamaria <anamaria.lupu@ppb.com>
2022-03-27 12:52:44 +0300

    homework2

---
+++ courses/modularity.py
@@ -0,0 +1,18 @@
+# if condition:
+#     print('condition is satisfied')
+# else:
+#     print('condition is not met')
+
+# age = 20
+# while age >= 18:
+#     print('Duplicate message')
+
+# print(range(10), list(range(10)))
+# print(range(10), list(range(5, 10)))
+# print(range(10), list(range(5, 10, 2))) #2 inseamna din 2 in 2
+
+# for element in sequence:
+#     print ('whatever')
+
+for number in range(3):
+    print(f'{number + 1}. duplicate message')
