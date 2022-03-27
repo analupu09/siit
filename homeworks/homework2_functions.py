ff23a52b5b45f4f58a993291b2a93c0026c0d808
Anamaria <anamaria.lupu@ppb.com>
2022-03-27 12:53:19 +0300

    homework2

---
+++ homeworks/homework2_functions.py
@@ -0,0 +1,40 @@
+# def my_sum(a, b, c, d, e):
+#     return a + b + c
+#
+#
+# print(my_sum(1, 5, -3, 'abc', [12, 56, 'cad']))
+
+
+# def my_parameter(a):
+#     print(a)
+#
+#
+# my_parameter(0)
+
+
+# def my_function(a, b, c, d):
+#     return a + b
+#
+#
+# print(my_function(2, 4, 'abc', d=2))
+
+
+# def total_numbers(n):
+#     if n == 0:
+#         return 0
+#
+#     return n + total_numbers(n - 1)
+#
+#
+# print(total_numbers(9))
+
+
+# def total_numbers(n):
+#     if n == 0:
+#         return 0
+#
+#     return n + total_numbers(n - 2)
+#
+#
+# print(total_numbers(8))
+
