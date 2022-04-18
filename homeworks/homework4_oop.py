32288d1e46f8351deab60aa156eb9cb40024cdc7
Anamaria <anamaria.lupu@ppb.com>
2022-04-18 19:25:58 +0300

    adding inverse function

--- homeworks/homework4_oop.py
+++ homeworks/homework4_oop.py
@@ -5,38 +5,55 @@
 # __sub__ : returnam o noua fractie care reprezinta scaderea
 # inverse: returneaza o noua fractie (inversa fractiei)
 
-from fractions import *
+
+# Euclid’s Algorithm states that the greatest common divisor of two integers numerator and denominator is denominator if denominator divides numerator evenly. However, if denominator does not divide numerator evenly, then the answer is the greatest common divisor of denominator and the remainder of nominator divided by denominator.
+def euclid_algorithm(numerator, denominator):
+    while numerator % denominator != 0:
+        old_numerator = numerator
+        old_denominator = denominator
+        numerator = old_denominator
+        denominator = old_numerator % old_denominator
+    return denominator
 
 
-class Fractie:
+class Fraction:
 
-    def __init__(self, numarator, numitor):
-        self.num = numarator
-        self.den = numitor
+    @staticmethod
+    def inverse(numerator, denominator):
+        return Fraction(denominator, numerator)
+
+    def __init__(self, numerator, denominator):
+        self.num = numerator
+        self.den = denominator
 
     def __str__(self):
         return str(self.num) + "/" + str(self.den)
 
-    def __add__(self, otherfraction):
-        newnum = self.num * otherfraction.den + self.den * otherfraction.num
-        newden = self.den * otherfraction.den
-        return Fractie(newnum, newden)
+    def __add__(self, other_fraction):
+        newnum = self.num * other_fraction.den + self.den * other_fraction.num
+        newden = self.den * other_fraction.den
+        common = euclid_algorithm(newnum, newden)
+        return Fraction(newnum // common, newden // common)
 
-    def __sub__(self, otherfraction):
-        newnum = self.num * otherfraction.den - self.den * otherfraction.num
-        newden = self.den * otherfraction.den
-        return Fractie(newnum, newden)
+    def __sub__(self, other_fraction):
+        newnum = self.num * other_fraction.den - self.den * other_fraction.num
+        newden = self.den * other_fraction.den
+        common = euclid_algorithm(newnum, newden)
+        return Fraction(newnum // common, newden // common)
 
 
-fraction = Fractie('numarator', 'numitor')
-print('Exemplu afisare metoda __str__', fraction)
+fraction = Fraction('numerator', 'denominator')
+print('Example of the fraction itself:', fraction)
 
-first_fraction = Fractie(1, 4)
-second_fraction = Fractie(1, 2)
+first_fraction = Fraction(1, 4)
+second_fraction = Fraction(1, 2)
 adding_fractions = first_fraction + second_fraction
-print('Fractia care reprezinta adunarea:', adding_fractions)
+print('Result of adding fractions:', adding_fractions)
 
-third_fraction = Fractie(3, 9)
-forth_fraction = Fractie(1, 7)
+third_fraction = Fraction(3, 9)
+forth_fraction = Fraction(1, 7)
 deduction_fractions = third_fraction - forth_fraction
-print('Fractia care reprezinta scaderea:', deduction_fractions)
+print('Result of deducting fraction:', deduction_fractions)
+
+print('Inverse fraction: ', Fraction.inverse(7, 9))
+
